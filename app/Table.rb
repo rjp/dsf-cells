3c6d944bad99904278f176e994559c434f4cd546
rjp <rjpartington@googlemail.com>
2012-11-13 17:50:23 +0000

    Commentary

--- app/Table.rb
+++ app/Table.rb
@@ -38,82 +38,96 @@ class Table < UITableViewController
         self.title = 'dsf'
         @table = self.tableView
         @table.dataSource = self
+
+        # We fake up 26 cells of data
         @data = ('A'..'Z').to_a
         @words = []
         @heights = []
 
+        # Make 26 blocks of text with varying sizes
         @data.each do
             w = ipsum()
             @words.push w
             @heights.push real_height(w, 256).height
         end
-        p @heights
 
+        # We all love a beardy ginger
         @dsfpic = UIImage.imageNamed('small-darrenf.jpg')
     end
 
     def tableView(tableView, heightForRowAtIndexPath: indexPath)
-        cell_id = @data[indexPath.row]
         # 90.0 is our base cell size, 52.0 is our text box
-        q = [90.0, 90.0-52.0+@heights[indexPath.row]].max
-        return q
+        return [90.0, 90.0-52.0+@heights[indexPath.row]].max
     end
 
     def tableView(tableView, cellForRowAtIndexPath: indexPath)
+        # Originally I was making this specific for each cell but that
+        # seemed to go horribly wrong. Not sure I understand why.
         cell_id = "CELL_ID"
 
         # return the UITableViewCell for the row
         @reuseIdentifier = cell_id
 
         cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+            # create a blank new cell with our identifier
             x = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
-            # height of our text box - starts at 52.0, expands with text
 
+            # Top left is an image
             image = UIImageView.alloc.init
             image.frame = [[8,8], [44,44]]
             image.image = @dsf_pic
             image.setTag(8)
             x.contentView.addSubview(image)
 
+            # Top right is a label pretending to be a UITextView
+            # because we don't want any scrolling behaviour
             topright = UILabel.alloc.init
             topright.setFont(font_10)
             topright.setTag(9)
+            # "as many lines as can fit in the space"
             topright.numberOfLines = 0
             x.contentView.addSubview(topright)
 
+            # Bottom left is a small label for something like a timestamp
             botleft = UILabel.alloc.init
             botleft.setFont(font_10)
             botleft.setTag(10)
             x.contentView.addSubview(botleft)
 
+            # Bottom right is the price of cheese
             botright = UILabel.alloc.init
             botright.backgroundColor = UIColor.lightGrayColor
             botright.setFont(font_10)
             botright.setTag(11)
             x.contentView.addSubview(botright)
 
+            # "return" the new cell object
             x
         end
 
+        # Resize and reposition according to the size of our text
         q = [52, @heights[indexPath.row]].max
         cell.viewWithTag(9).frame = [[56,8], [256,q]]
         cell.viewWithTag(10).frame = [[8,q+12], [80,20]]
         cell.viewWithTag(11).frame = [[92,q+12], [220,20]]
 
+        # Since this can vary with each cell, we have to set it here
         cell.viewWithTag(8).image = @dsfpic
 
-        # botleft/10
+        # Ditto
         cell.viewWithTag(10).text = '[' + @data[indexPath.row] + '] Mingle Mangle'
-        # topright/9
+        # Ditto
         cell.viewWithTag(9).text = @words[indexPath.row]
-        # botright/11
+
+        # You get the idea
         cell.viewWithTag(11).text = @heights[indexPath.row].to_s + ' /' + @data[indexPath.row] + '/ ' + @words[indexPath.row].length.to_s
 
+        # Return our cell for displaying
         cell
     end
 
+    # We always have the same number of rows but let's pretend we don't
     def tableView(tableView, numberOfRowsInSection: section)
-        # return the number of rows
         @data.count
     end
 end
